ac46d0ea3da47bc3aca69bdcdd4b4823e673572f
Dave Aronson <davearonson@users.noreply.github.com>
2023-09-09 13:17:10 -0400

    Setup Gigalixir deployment with Elixir Releases

--- config/runtime.exs
+++ config/runtime.exs
@@ -15,7 +15,7 @@ if config_env() == :prod do
       """
 
   config :sample_app, SampleApp.Repo,
-    # ssl: true,
+    ssl: true,
     # socket_options: [:inet6],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
@@ -33,14 +33,9 @@ if config_env() == :prod do
       """
 
   config :sample_app, SampleAppWeb.Endpoint,
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
-    ],
+    server: true,
+    http: [port: {:system, "PORT"}],
+    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
     secret_key_base: secret_key_base
 
   # ## Using releases
--- lib/sample_app_web/controllers/page_controller.ex
+++ lib/sample_app_web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule SampleAppWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def hello(conn, _params) do
+    html(conn, "hello, world!")
+  end
 end
