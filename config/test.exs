4db450b646479de422d348319efb364ea489f358
Dave Aronson <davearonson@users.noreply.github.com>
2023-09-09 16:27:24 -0400

    tweak static pages, and add About

--- config/test.exs
+++ config/test.exs
@@ -24,7 +24,7 @@ config :sample_app, SampleAppWeb.Endpoint,
 config :sample_app, SampleApp.Mailer, adapter: Swoosh.Adapters.Test
 
 # Print only warnings and errors during test
-config :logger, level: :warn
+config :logger, level: :warning
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
