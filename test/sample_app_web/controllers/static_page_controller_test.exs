cadcd97b5ade2e9affc38a4539fab2e6787ba2c6
Dave Aronson <davearonson@users.noreply.github.com>
2023-09-09 16:35:19 -0400

    add browser tab titles

--- test/sample_app_web/controllers/static_page_controller_test.exs
+++ test/sample_app_web/controllers/static_page_controller_test.exs
@@ -5,17 +5,26 @@ defmodule SampleAppWeb.StaticPageControllerTest do
     conn = get(conn, Routes.static_page_path(conn, :home))
 
     assert html_response(conn, 200)
+
+    html_response(conn, 200)
+    |> assert_select("title", "Home | Phoenix Tutorial Sample App")
   end
 
   test "should get help", %{conn: conn} do
     conn = get(conn, Routes.static_page_path(conn, :help))
 
     assert html_response(conn, 200)
+
+    html_response(conn, 200)
+    |> assert_select("title", "Help | Phoenix Tutorial Sample App")
   end
 
   test "should get about", %{conn: conn} do
     conn = get(conn, Routes.static_page_path(conn, :about))
 
     assert html_response(conn, 200)
+
+    html_response(conn, 200)
+    |> assert_select("title", "About | Phoenix Tutorial Sample App")
   end
 end
